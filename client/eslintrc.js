a4fb1c7ebc58510630275a02f6d1acc7510841e4
lily-law <lilylaw.dev@gmail.com>
2020-10-29 12:03:28 +0000

    Apply npm run format for first time

--- client/eslintrc.js
+++ client/eslintrc.js
@@ -1,44 +1,44 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-      ecmaVersion: 2020,
-      sourceType: 'module',
-      ecmaFeatures: {
-          jsx: true
-      }
-  },
-  settings: {
-      react: {
-          version: 'detect'
-      }
-  },
-  env: {
-      browser: true,
-      amd: true,
-      node: true
-  },
-  extends: [
-      'eslint:recommended',
-      'plugin:react/recommended',
-      'plugin:jsx-a11y/recommended',
-      'plugin:@typescript-eslint/eslint-recommended',
-      'plugin:@typescript-eslint/recommended',
-      'prettier/@typescript-eslint',
-      'plugin:prettier/recommended' // Make this the last element so prettier config overrides other formatting rules
-  ],
-  rules: {
-    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
-    'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    'simple-import-sort/sort': 'error',
-    'jsx-a11y/anchor-is-valid': [
-        'error',
-        {
-            components: ['Link'],
-            specialLink: ['hrefLeft', 'hrefRight'],
-            aspects: ['invalidHref', 'preferButton']
-        }
-    ]
-  }
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+    env: {
+        browser: true,
+        amd: true,
+        node: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:jsx-a11y/recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended', // Make this the last element so prettier config overrides other formatting rules
+    ],
+    rules: {
+        'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+        'react/react-in-jsx-scope': 'off',
+        'react/prop-types': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        'simple-import-sort/sort': 'error',
+        'jsx-a11y/anchor-is-valid': [
+            'error',
+            {
+                components: ['Link'],
+                specialLink: ['hrefLeft', 'hrefRight'],
+                aspects: ['invalidHref', 'preferButton'],
+            },
+        ],
+    },
 }
