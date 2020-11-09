ead217718d8be04a23b2aa2c13e810dc663c69e7
lily-law <lilylaw.dev@gmail.com>
2020-11-09 11:18:53 +0000

    Setup mongoose

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common'
 import { UserModule } from './user/user.module'
 import { GameModule } from './game/game.module'
+import { MongooseModule } from '@nestjs/mongoose'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 
 @Module({
-    imports: [UserModule, GameModule]
+    imports: [
+        UserModule,
+        GameModule,
+        MongooseModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: async (configService: ConfigService) => ({
+                uri: configService.get<string>('MONGODB_URI')
+            }),
+            inject: [ConfigService]
+        }),
+        ConfigModule.forRoot()
+    ]
 })
 export class AppModule {}
